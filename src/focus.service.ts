4d0b5a7538f5e931de88f161cf08c5083982b3d5
saadataz <saadat.azim@outlook.com>
2017-06-01 13:37:57 -0700

    prevent duplicate calls to selectNode (#64)

--- src/focus.service.ts
+++ src/focus.service.ts
@@ -236,6 +236,12 @@ function isNodeAttached(node: HTMLElement, root: HTMLElement) {
 export class FocusService {
   public enableRaycast = true;
   public focusedClass = 'arc--selected-direct';
+
+  /**
+   * Animation speed in pixels per second for scrolling elements into view.
+   * This can be Infinity to disable the animation, or null to disable scrolling.
+   */
+  public scrollSpeed = 1000;
   // Focus root, the service operates below here.
   private root: HTMLElement;
   public focusRoot: HTMLElement = defaultFocusRoot;
