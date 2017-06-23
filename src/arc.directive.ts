c3e21c926855010e8e93d00a2f3c4cb3edf02166
Saadat Azim <saazim@microsoft.com>
2017-06-22 18:20:15 -0700

    add ngFor support for arc-default-focus

--- src/arc.directive.ts
+++ src/arc.directive.ts
@@ -48,7 +48,10 @@ export class ArcDirective implements OnInit, OnDestroy, IArcHandler {
   public arcFocus = new EventEmitter<HTMLElement>();
 
   @Input('arc-default-focus')
-  public set arcDefaultFocus(_ignored: any) {
+  public set arcDefaultFocus(shouldFocus: boolean) {
+    if (shouldFocus === false) {
+      return;
+    }
     this.arcSetFocus = this.arcSetFocus.startWith(undefined);
   }
 
