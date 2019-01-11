671bf21cb28debe99e0cc626f90b6f13ebcffc59
xiaoxli <36868685+xiaoxli@users.noreply.github.com>
2019-01-11 15:41:44 -0800

    fix: ignore interface prefix error

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,8 @@
-interface ScrollOptions {
+interface IScrollOptions {
   preventScroll: boolean;
 }
+
+// tslint:disable-next-line
 interface HTMLElement {
-  focus(scrollOptions?: ScrollOptions): void;
+  focus(scrollOptions?: IScrollOptions): void;
 }
